118c5ac7eebe5422e44c79258ae48eab2dc20ea1
hachi-nico <nicokun48@gmail.com>
2022-10-07 22:41:52 +0700

    load more

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
 
+import Button from '@mui/material/Button'
 import TableRow from '@mui/material/TableRow'
 import TableCell from '@mui/material/TableCell'
 
@@ -10,6 +11,7 @@ import GlobalAlert from '../components/GlobalAlert'
 import FullScreenLoader from '../components/FullScreenLoader'
 import {getLocalToken} from '../utils/globalFunction'
 import {baseUrl} from '../config/api'
+import {indigo} from '../config/color'
 
 const UserPage = () => {
   const [userList, setUserList] = useState([])
@@ -26,43 +28,55 @@ const UserPage = () => {
     setMultiState(setPageState, {loading: true})
     const controller = new AbortController()
     const init = async () => {
-      try {
-        const {data} = await axios.post(
-          baseUrl + 'user-list',
-          {limit, offset},
-          {
-            headers: {
-              authorization: getLocalToken(),
-            },
-          }
-        )
-        if (data.status == 1) {
-          setMultiState(setPageState, {loading: false})
-          setUserList(data.users)
-        } else {
-          setMultiState(setPageState, {
-            loading: false,
-            err: true,
-            errMsg: 'Gagal saat mengambil data list User',
-          })
-        }
-      } catch (e) {
-        setMultiState(setPageState, {
-          loading: false,
-          err: true,
-          errMsg: '[CEA] - Gagal saat mengambil data list User',
-        })
-      }
+      await fetchUser()
     }
     init()
     return () => controller.abort()
-  }, [limit, offset])
+  }, [])
 
   const setMultiState = (setStateName, obj) => {
     setStateName(s => ({
       ...s,
       ...obj,
     }))
+  }
+
+  const fetchUser = async (isFetchmore = false) => {
+    try {
+      const {data} = await axios.post(
+        baseUrl + 'user-list',
+        {limit, offset},
+        {
+          headers: {
+            authorization: getLocalToken(),
+          },
+        }
+      )
+      if (data.status == 1) {
+        const {users} = data
+        setMultiState(setPageState, {loading: false})
+        !isFetchmore ? setUserList(users) : setUserList(s =>  [...s, ...users])
+      } else {
+        setMultiState(setPageState, {
+          loading: false,
+          err: true,
+          errMsg: 'Gagal saat mengambil data list User',
+        })
+      }
+    } catch (e) {
+      setMultiState(setPageState, {
+        loading: false,
+        err: true,
+        errMsg: '[CEA] - Gagal saat mengambil data list User',
+      })
+    }
+  }
+
+  const fetchMore = async dataLength => {
+    setMultiState(setPageState, {
+      offset: dataLength,
+    })
+    await fetchUser(true)
   }
 
   return (
@@ -78,19 +92,32 @@ const UserPage = () => {
       <PlainCard label="Manajemen User" />
       <div style={{marginTop: 26}}>
         {!err && !loading && userList.length > 0 ? (
-          <GlobalTable headingList={['No.', 'Nama', 'Username', 'Tipe']}>
-            {userList.map((item, i) => (
-              <TableRow
-                key={i}
-                sx={{'&:last-child td, &:last-child th': {border: 0}}}
+          <>
+            <GlobalTable headingList={['No.', 'Nama', 'Username', 'Tipe']}>
+              {userList.map((item, i) => (
+                <TableRow
+                  key={i}
+                  sx={{'&:last-child td, &:last-child th': {border: 0}}}
+                >
+                  <TableCell>{item.user_id}</TableCell>
+                  <TableCell>{item.nama}</TableCell>
+                  <TableCell>{item.username}</TableCell>
+                  <TableCell>{item.tipe}</TableCell>
+                </TableRow>
+              ))}
+            </GlobalTable>
+            <div
+              style={{display: 'flex', flexDirection: 'column', marginTop: 20}}
+            >
+              <Button
+                sx={{p: 1, backgroundColor: indigo}}
+                variant="contained"
+                onClick={() => fetchMore(userList.length)}
               >
-                <TableCell>{i + 1}</TableCell>
-                <TableCell>{item.nama}</TableCell>
-                <TableCell>{item.username}</TableCell>
-                <TableCell>{item.tipe}</TableCell>
-              </TableRow>
-            ))}
-          </GlobalTable>
+                Muat lagi
+              </Button>
+            </div>
+          </>
         ) : null}
       </div>
     </>
